8dd1827332c4ba8d28a0f814b10301e229b8793e
Greg Williamson <cheeseboy16@gmail.com>
2021-03-12 06:43:24 -0500

    codegen fix

--- enigma/Android.mk
+++ enigma/Android.mk
@@ -10,7 +10,7 @@ LOCAL_MODULE := enigma
 LOCAL_C_INCLUDES := $(ENIGMA_SHELL) $(EXTERNAL)/hacks $(EXTERNAL)/libepoxy/include/
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES) 
 
-LOCAL_C_INCLUDES += $(ENIGMA_ROOT)/shared $(ENIGMA_SHELL)/Bridges/OpenGLES/ $(EXTERNAL)/SDL2/include/ $(EXTERNAL)/glad/gles2/include $(EXTERNAL)/glm $(ENIGMA_SHELL)/Platforms/SDL/fileio /tmp/ENIGMA
+LOCAL_C_INCLUDES += $(ENIGMA_ROOT)/shared $(ENIGMA_SHELL)/Bridges/OpenGLES/ $(EXTERNAL)/SDL2/include/ $(EXTERNAL)/glad/gles2/include $(EXTERNAL)/glm $(ENIGMA_SHELL)/Platforms/SDL/fileio $(CODEGEN)
 LOCAL_CFLAGS := -DDEBUG_MODE -DENIGMA_PLATFORM_SDL
 LOCAL_CPPFLAGS := -stdlib=libc++ -std=c++17
 
